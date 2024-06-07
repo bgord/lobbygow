91b7c45dc3788bc0892a7af9c5a9d8ece0f9cc51
Bartosz Gordon <gordonbartosz@gmail.com>
2024-06-07 16:51:23 +0200

    feature: make env schema stricter

--- infra/env.ts
+++ infra/env.ts
@@ -1,20 +1,22 @@
 import * as bg from "@bgord/node";
 import { z } from "zod";
 
-const EnvironmentSchema = z.object({
-  PORT: bg.Schema.Port,
-  LOGS_LEVEL: bg.Schema.LogLevel,
-  SMTP_HOST: bg.Schema.SmtpHost,
-  SMTP_PORT: bg.Schema.SmtpPort,
-  SMTP_USER: bg.Schema.SmtpUser,
-  SMTP_PASS: bg.Schema.SmtpPass,
-  EMAIL_FROM: bg.Schema.Email,
-  EMAIL_TO: bg.Schema.EmailTo,
-  TZ: bg.Schema.TimezoneUTC,
-  BASIC_AUTH_USERNAME: bg.Schema.BasicAuthUsername,
-  BASIC_AUTH_PASSWORD: bg.Schema.BasicAuthPassword,
-  API_KEY: bg.Schema.ApiKey,
-});
+const EnvironmentSchema = z
+  .object({
+    PORT: bg.Schema.Port,
+    LOGS_LEVEL: bg.Schema.LogLevel,
+    SMTP_HOST: bg.Schema.SmtpHost,
+    SMTP_PORT: bg.Schema.SmtpPort,
+    SMTP_USER: bg.Schema.SmtpUser,
+    SMTP_PASS: bg.Schema.SmtpPass,
+    EMAIL_FROM: bg.Schema.Email,
+    EMAIL_TO: bg.Schema.EmailTo,
+    TZ: bg.Schema.TimezoneUTC,
+    BASIC_AUTH_USERNAME: bg.Schema.BasicAuthUsername,
+    BASIC_AUTH_PASSWORD: bg.Schema.BasicAuthPassword,
+    API_KEY: bg.Schema.ApiKey,
+  })
+  .strict();
 type EnvironmentSchemaType = z.infer<typeof EnvironmentSchema>;
 
 export const Env = new bg.EnvironmentValidator<EnvironmentSchemaType>({

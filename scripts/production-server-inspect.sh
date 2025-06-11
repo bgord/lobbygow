c1dc56bfc4c065e892627e5574ceb5ea274562de
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-11 22:07:37 +0200

    step: add inspect app size script

---
+++ scripts/production-server-inspect.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Preload base bash configuration and functions
+source bgord-scripts/base.sh
+setup_base_config
+
+OUTPUT_DIRECTORY="build"
+
+du -sh "$OUTPUT_DIRECTORY"/lobbygow

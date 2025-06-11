a951d83cd24251624bcfcbfe1022ddb6993bb43a
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-11 13:18:02 +0200

    step: add validation tests

--- test/notification-send.test.ts
+++ test/notification-send.test.ts
@@ -6,8 +6,70 @@ import { server } from "../server";
 import * as testcases from "./testcases";
 
 describe("POST /notification-send", () => {
+  test("validation - empty payload", async () => {
+    const response = await server.request(
+      "/notification-send",
+      {
+        method: "POST",
+        headers: new Headers({
+          [bgb.ApiKeyShield.HEADER_NAME]: infra.Env.API_KEY,
+        }),
+      },
+      testcases.ip,
+    );
+
+    const json = await response.json();
+
+    expect(response.status).toBe(400);
+    expect(json).toEqual({ message: "payload.invalid.error", _known: true });
+  });
+
+  test("validation - missing subject", async () => {
+    const response = await server.request(
+      "/notification-send",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          content: "content",
+          kind: Mailer.VO.NotificationKindEnum.info,
+        }),
+        headers: new Headers({
+          [bgb.ApiKeyShield.HEADER_NAME]: infra.Env.API_KEY,
+        }),
+      },
+      testcases.ip,
+    );
+
+    const json = await response.json();
+
+    expect(response.status).toBe(400);
+    expect(json).toEqual({ message: "payload.invalid.error", _known: true });
+  });
+
+  test("validation - missing content", async () => {
+    const response = await server.request(
+      "/notification-send",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          subject: "subject",
+          kind: Mailer.VO.NotificationKindEnum.info,
+        }),
+        headers: new Headers({
+          [bgb.ApiKeyShield.HEADER_NAME]: infra.Env.API_KEY,
+        }),
+      },
+      testcases.ip,
+    );
+
+    const json = await response.json();
+
+    expect(response.status).toBe(400);
+    expect(json).toEqual({ message: "payload.invalid.error", _known: true });
+  });
+
   test("happy path", async () => {
-    const res = await server.request(
+    const response = await server.request(
       "/notification-send",
       {
         method: "POST",
@@ -20,9 +82,28 @@ describe("POST /notification-send", () => {
           [bgb.ApiKeyShield.HEADER_NAME]: infra.Env.API_KEY,
         }),
       },
-      ip,
+      testcases.ip,
     );
 
-    expect(res.status).toBe(200);
+    expect(response.status).toBe(200);
+  });
+
+  test("happy path - default kind", async () => {
+    const response = await server.request(
+      "/notification-send",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          subject: "subject",
+          content: "content",
+        }),
+        headers: new Headers({
+          [bgb.ApiKeyShield.HEADER_NAME]: infra.Env.API_KEY,
+        }),
+      },
+      testcases.ip,
+    );
+
+    expect(response.status).toBe(200);
   });
 });

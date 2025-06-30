a50967429c0dd93187d828b45ad24f9b97a152e3
Bartosz Gordon <gordonbartosz@gmail.com>
2025-06-30 19:59:06 +0200

    step: add axiom transport to logger

--- infra/logger.ts
+++ infra/logger.ts
@@ -1,8 +1,14 @@
 import * as bg from "@bgord/bun";
 import { Env } from "./env";
 
+const app = "lobbygow";
+
 export const logger = new bg.Logger({
-  app: "lobbygow",
+  app,
   environment: Env.type,
   level: Env.LOGS_LEVEL,
+  transports:
+    Env.type === bg.NodeEnvironmentEnum.production
+      ? [new bg.AxiomTransport({ dataset: app, token: Env.AXIOM_API_TOKEN })]
+      : [],
 });

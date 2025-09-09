2e9a52387c43711b0ed9a19ea2bd876e499f3328
Bartosz Gordon <gordonbartosz@gmail.com>
2025-09-09 19:47:39 +0200

    refactor: simplify size

--- infra/prerequisites.ts
+++ infra/prerequisites.ts
@@ -5,19 +5,13 @@ import { LoggerWinstonProductionAdapter } from "./logger.adapter";
 
 export const prerequisites = [
   new bg.PrerequisitePort({ label: "port", port: Env.PORT }),
-  new bg.PrerequisiteTimezoneUTC({
-    label: "timezone",
-    timezone: tools.Timezone.parse(Env.TZ),
-  }),
+  new bg.PrerequisiteTimezoneUTC({ label: "timezone", timezone: tools.Timezone.parse(Env.TZ) }),
   new bg.PrerequisiteRAM({
     label: "RAM",
+    minimum: tools.Size.fromMB(128),
     enabled: Env.type !== bg.NodeEnvironmentEnum.local,
-    minimum: new tools.Size({ unit: tools.SizeUnit.MB, value: 128 }),
   }),
-  new bg.PrerequisiteSpace({
-    label: "disk-space",
-    minimum: new tools.Size({ unit: tools.SizeUnit.MB, value: 512 }),
-  }),
+  new bg.PrerequisiteSpace({ label: "disk-space", minimum: tools.Size.fromMB(512) }),
   new bg.PrerequisiteNode({
     label: "node",
     version: tools.PackageVersion.fromStringWithV("v24.1.0"),
@@ -25,14 +19,10 @@ export const prerequisites = [
   }),
   new bg.PrerequisiteBun({
     label: "bun",
-    enabled: false, // TODO
     version: tools.PackageVersion.fromString("1.2.20"),
     current: Bun.version,
   }),
-  new bg.PrerequisiteMemory({
-    label: "memory-consumption",
-    maximum: new tools.Size({ value: 300, unit: tools.SizeUnit.MB }),
-  }),
+  new bg.PrerequisiteMemory({ label: "memory-consumption", maximum: tools.Size.fromMB(300) }),
   new bg.PrerequisiteLogFile({
     label: "log-file",
     logger: LoggerWinstonProductionAdapter,

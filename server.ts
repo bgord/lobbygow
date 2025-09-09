1178c03772ec228b66c274606733f6c5d4b90994
Bartosz Gordon <gordonbartosz@gmail.com>
2025-09-09 14:56:40 +0200

    refactor: add server deps

--- server.ts
+++ server.ts
@@ -3,45 +3,57 @@ import * as tools from "@bgord/tools";
 import { Hono } from "hono";
 import { timeout } from "hono/timeout";
 import * as infra from "+infra";
+import { Clock } from "+infra/clock.adapter";
 import { Env } from "+infra/env";
 import { healthcheck } from "+infra/healthcheck";
 import { I18nConfig } from "+infra/i18n";
-import { logger } from "+infra/logger.adapter";
+import { IdProvider } from "+infra/id-provider";
+import { Logger } from "+infra/logger.adapter";
 import * as RateLimiters from "+infra/rate-limiters";
 import { ShieldApiKey } from "+infra/shield-api-key";
 import { ShieldBasicAuth } from "+infra/shield-basic-auth";
 import * as App from "./app";
 
+const ShieldRateLimitDeps = { Clock };
+const HealthcheckDeps = { Clock };
+const ServerDeps = { Logger, I18n: I18nConfig, Clock, IdProvider };
+
 type HonoConfig = { Variables: infra.Variables; startup: tools.Stopwatch };
 
 const server = new Hono<HonoConfig>();
 
-server.use(...bg.Setup.essentials(logger, I18nConfig));
+server.use(...bg.Setup.essentials(ServerDeps));
 
-const startup = new tools.Stopwatch();
+const startup = new tools.Stopwatch(Clock.nowMs());
 
 // Healthcheck =================
 server.get(
   "/healthcheck",
-  bg.ShieldRateLimit({
-    enabled: Env.type === bg.NodeEnvironmentEnum.production,
-    subject: bg.AnonSubjectResolver,
-    store: RateLimiters.HealthcheckStore,
-  }),
+  bg.ShieldRateLimit(
+    {
+      enabled: Env.type === bg.NodeEnvironmentEnum.production,
+      subject: bg.AnonSubjectResolver,
+      store: RateLimiters.HealthcheckStore,
+    },
+    ShieldRateLimitDeps,
+  ),
   timeout(tools.Time.Seconds(15).ms, infra.requestTimeoutError),
   ShieldBasicAuth,
-  ...bg.Healthcheck.build(healthcheck),
+  ...bg.Healthcheck.build(healthcheck, HealthcheckDeps),
 );
 // =============================
 
 // Mailer =================
 server.post(
   "/notification-send",
-  bg.ShieldRateLimit({
-    enabled: Env.type === bg.NodeEnvironmentEnum.production,
-    subject: bg.AnonSubjectResolver,
-    store: RateLimiters.NotificationSendStore,
-  }),
+  bg.ShieldRateLimit(
+    {
+      enabled: Env.type === bg.NodeEnvironmentEnum.production,
+      subject: bg.AnonSubjectResolver,
+      store: RateLimiters.NotificationSendStore,
+    },
+    ShieldRateLimitDeps,
+  ),
   timeout(tools.Time.Seconds(15).ms, infra.requestTimeoutError),
   ShieldApiKey.verify,
   App.Http.Mailer.NotificationSend,

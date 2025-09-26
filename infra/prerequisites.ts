f4e0e6d52475d1fced7f1ae0be9b69a55b7b67fe
Bartosz Gordon <gordonbartosz@gmail.com>
2025-09-26 13:49:12 +0200

    refactor: restore previous bun version

--- infra/prerequisites.ts
+++ infra/prerequisites.ts
@@ -18,7 +18,7 @@ export const prerequisites = [
   }),
   new bg.PrerequisiteBun({
     label: "bun",
-    version: tools.PackageVersion.fromString("1.2.22"),
+    version: tools.PackageVersion.fromString("1.2.20"),
     current: Bun.version,
   }),
   new bg.PrerequisiteMemory({ label: "memory-consumption", maximum: tools.Size.fromMB(300) }),

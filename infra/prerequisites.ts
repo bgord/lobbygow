02bdc8c3c0c2da9ab70c9c915258bfb5e5320e84
Bartosz Gordon <gordonbartosz@gmail.com>
2025-09-26 13:46:31 +0200

    refactor: simplify node version in prereq

--- infra/prerequisites.ts
+++ infra/prerequisites.ts
@@ -13,7 +13,7 @@ export const prerequisites = [
   new bg.PrerequisiteSpace({ label: "disk-space", minimum: tools.Size.fromMB(512) }),
   new bg.PrerequisiteNode({
     label: "node",
-    version: tools.PackageVersion.fromStringWithV("v24.1.0"),
+    version: tools.PackageVersion.fromString("24.1.0"),
     current: process.version,
   }),
   new bg.PrerequisiteBun({

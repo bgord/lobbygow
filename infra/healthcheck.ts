11535f4ebd1552650bfe7103aa6b91d0f16d4a32
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-04 21:29:42 +0100

    refactor: cleanup healthcheck

--- infra/healthcheck.ts
+++ infra/healthcheck.ts
@@ -1,10 +1,7 @@
 import * as bg from "@bgord/bun";
-import { Mailer } from "./mailer.adapter";
 import { prerequisites } from "./prerequisites";
 
 export const healthcheck = [
   new bg.PrerequisiteSelf({ label: "self" }),
-  new bg.PrerequisiteOutsideConnectivity({ label: "outside-connectivity" }),
-  new bg.PrerequisiteMailer({ label: "nodemailer", mailer: Mailer }),
   ...prerequisites.filter((prerequisite) => prerequisite.label !== "port"),
 ];

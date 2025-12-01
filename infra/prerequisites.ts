6a149126a3fa359ffdaf508d27ab4d2a9a8acbb8
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-01 11:51:55 +0100

    step: update bun

--- infra/prerequisites.ts
+++ infra/prerequisites.ts
@@ -25,7 +25,7 @@ export const prerequisites = [
   }),
   new bg.PrerequisiteBun({
     label: "bun",
-    version: tools.PackageVersion.fromString("1.3.2"),
+    version: tools.PackageVersion.fromString("1.3.3"),
     current: Bun.version,
   }),
   new bg.PrerequisiteMemory({ label: "memory-consumption", maximum: tools.Size.fromMB(300) }),

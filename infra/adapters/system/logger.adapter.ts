331ca43282338b3de91acdc20db4719e7e12dc56
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-12 19:38:10 +0100

    refactor: simplify logger adapter

--- infra/adapters/system/logger.adapter.ts
+++ infra/adapters/system/logger.adapter.ts
@@ -3,23 +3,22 @@ import type { EnvironmentType } from "+infra/env";
 
 export function createLogger(Env: EnvironmentType): bg.LoggerPort {
   const app = "lobbygow";
+
   const redactor = new bg.RedactorCompositeAdapter([
     new bg.RedactorCompactArrayAdapter(),
     new bg.RedactorMaskAdapter(bg.RedactorMaskAdapter.DEFAULT_KEYS),
   ]);
-  const LoggerWinstonLocalAdapter = new bg.LoggerWinstonLocalAdapter({ app, redactor }).create(
-    Env.LOGS_LEVEL,
-  );
-  const LoggerWinstonProductionAdapter = new bg.LoggerWinstonProductionAdapter({
-    app,
-    AXIOM_API_TOKEN: Env.AXIOM_API_TOKEN,
-    redactor,
-  });
 
   return {
-    [bg.NodeEnvironmentEnum.local]: LoggerWinstonLocalAdapter,
+    [bg.NodeEnvironmentEnum.local]: new bg.LoggerWinstonLocalAdapter({ app, redactor }).create(
+      Env.LOGS_LEVEL,
+    ),
     [bg.NodeEnvironmentEnum.test]: new bg.LoggerNoopAdapter(),
     [bg.NodeEnvironmentEnum.staging]: new bg.LoggerNoopAdapter(),
-    [bg.NodeEnvironmentEnum.production]: LoggerWinstonProductionAdapter.create(Env.LOGS_LEVEL),
+    [bg.NodeEnvironmentEnum.production]: new bg.LoggerWinstonProductionAdapter({
+      app,
+      AXIOM_API_TOKEN: Env.AXIOM_API_TOKEN,
+      redactor,
+    }).create(Env.LOGS_LEVEL),
   }[Env.type];
 }

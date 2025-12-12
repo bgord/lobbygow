b69ad5008eed9adac7aea575fa26b6018495ef53
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-12 19:27:21 +0100

    refactor: remove notifier specific shield rate limit

--- infra/bootstrap.ts
+++ infra/bootstrap.ts
@@ -1,4 +1,3 @@
-import { createNotifierAdapters } from "+infra/adapters/notifier";
 import { createSystemAdapters } from "+infra/adapters/system";
 import type { EnvironmentType } from "+infra/env";
 import { I18nConfig } from "+infra/i18n";
@@ -6,9 +5,8 @@ import { createPrerequisites } from "+infra/prerequisites";
 
 export async function bootstrap(Env: EnvironmentType) {
   const System = createSystemAdapters(Env);
-  const Notifier = createNotifierAdapters(Env, System);
 
   const prerequisites = createPrerequisites(Env, System);
 
-  return { Env, Adapters: { System, Notifier }, Tools: { prerequisites, I18nConfig } };
+  return { Env, Adapters: { System }, Tools: { prerequisites, I18nConfig } };
 }

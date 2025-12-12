3175f4fe87b29f4c799210f54323e11c60a9f9a2
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-12 19:01:16 +0100

    refactor: define port return types

--- infra/adapters/system/shield-basic-auth.adapter.ts
+++ infra/adapters/system/shield-basic-auth.adapter.ts
@@ -1,7 +1,7 @@
 import * as bg from "@bgord/bun";
 import type { EnvironmentType } from "+infra/env";
 
-export function createShieldBasicAuth(Env: EnvironmentType) {
+export function createShieldBasicAuth(Env: EnvironmentType): bg.ShieldPort {
   return new bg.ShieldBasicAuthAdapter({
     username: Env.BASIC_AUTH_USERNAME,
     password: Env.BASIC_AUTH_PASSWORD,
--- infra/adapters/system/shield-timeout.adapter.ts
+++ infra/adapters/system/shield-timeout.adapter.ts
@@ -1,6 +1,6 @@
 import * as bg from "@bgord/bun";
 import * as tools from "@bgord/tools";
 
-export function createShieldTimeout() {
+export function createShieldTimeout(): bg.ShieldPort {
   return new bg.ShieldTimeoutAdapter({ duration: tools.Duration.Seconds(15) });
 }

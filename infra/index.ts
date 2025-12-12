480f945d5b854d8cd32ede745037f3eb5735a827
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-12 17:59:35 +0100

    refactor: remove request timeout error

--- infra/index.ts
+++ infra/index.ts
@@ -1,7 +1,4 @@
 import type * as bg from "@bgord/bun";
-import { HTTPException } from "hono/http-exception";
 import type { TimingVariables } from "hono/timing";
-
-export const requestTimeoutError = new HTTPException(408, { message: "request_timeout_error" });
 
 export type Variables = TimingVariables & bg.TimeZoneOffsetVariables & bg.ContextVariables & bg.EtagVariables;

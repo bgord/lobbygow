4a21f426a6f92898d1bedee6cd02d0affae86999
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-14 23:42:22 +0100

    refactor: adjust email

--- modules/notifier/services/notification.ts
+++ modules/notifier/services/notification.ts
@@ -4,7 +4,7 @@ import type { NotificationComposerStrategy } from "./notification-composer";
 
 export class Notification {
   constructor(
-    private readonly EMAIL_FROM: bg.EmailFromType,
+    private readonly EMAIL_FROM: tools.EmailType,
     private readonly subject: bg.EmailSubjectType,
     private readonly content: bg.EmailContentHtmlType,
     private readonly deps: { Mailer: bg.MailerPort },
@@ -14,7 +14,7 @@ export class Notification {
     return strategy.compose(this.subject, this.content);
   }
 
-  async send(message: tools.NotificationTemplate, to: bg.EmailToType) {
+  async send(message: tools.NotificationTemplate, to: tools.EmailType) {
     return this.deps.Mailer.send({ from: this.EMAIL_FROM, to, ...message.get() });
   }
 }

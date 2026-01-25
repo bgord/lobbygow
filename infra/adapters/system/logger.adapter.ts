39a0c21bdd008df5abaf4e8582f2a0ec7d4538d4
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-25 16:48:32 +0100

    refactor: use woodchopper

--- infra/adapters/system/logger.adapter.ts
+++ infra/adapters/system/logger.adapter.ts
@@ -1,20 +1,39 @@
 import * as bg from "@bgord/bun";
 import type { EnvironmentType } from "+infra/env";
 
-export function createLogger(Env: EnvironmentType): bg.LoggerPort {
-  const app = "lobbygow";
+type Dependencies = { Clock: bg.ClockPort };
 
+export function createLogger(Env: EnvironmentType, deps: Dependencies): bg.LoggerPort {
   const redactor = new bg.RedactorCompositeStrategy([
-    new bg.RedactorCompactArrayStrategy(),
+    new bg.RedactorMetadataCompactArrayStrategy(),
     new bg.RedactorMaskStrategy(bg.RedactorMaskStrategy.DEFAULT_KEYS),
   ]);
-
-  const config = { app, redactor };
+  const diagnostics = new bg.WoodchopperDiagnosticsConsoleError(redactor);
 
   return {
-    [bg.NodeEnvironmentEnum.local]: new bg.LoggerWinstonLocalAdapter(config).create(Env.LOGS_LEVEL),
+    [bg.NodeEnvironmentEnum.local]: new bg.Woodchopper(
+      {
+        app: "lobbygow",
+        environment: Env.type,
+        level: Env.LOGS_LEVEL,
+        redactor,
+        dispatcher: new bg.WoodchopperDispatcherAsync(new bg.WoodchopperSinkStdoutHuman()),
+        diagnostics,
+      },
+      deps,
+    ),
     [bg.NodeEnvironmentEnum.test]: new bg.LoggerNoopAdapter(),
     [bg.NodeEnvironmentEnum.staging]: new bg.LoggerNoopAdapter(),
-    [bg.NodeEnvironmentEnum.production]: new bg.LoggerWinstonProductionAdapter(config).create(Env.LOGS_LEVEL),
+    [bg.NodeEnvironmentEnum.production]: new bg.Woodchopper(
+      {
+        app: "lobbygow",
+        environment: Env.type,
+        level: Env.LOGS_LEVEL,
+        redactor,
+        dispatcher: new bg.WoodchopperDispatcherAsync(new bg.WoodchopperSinkStdout()),
+        diagnostics,
+      },
+      deps,
+    ),
   }[Env.type];
 }

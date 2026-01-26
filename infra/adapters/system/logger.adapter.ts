97d35d0357a9a2c41cdc8d82234889bda4369013
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-26 18:46:41 +0100

    step: use sampling

--- infra/adapters/system/logger.adapter.ts
+++ infra/adapters/system/logger.adapter.ts
@@ -1,24 +1,33 @@
 import * as bg from "@bgord/bun";
+import * as tools from "@bgord/tools";
 import type { EnvironmentType } from "+infra/env";
 
 type Dependencies = { Clock: bg.ClockPort };
 
-export function createLogger(Env: EnvironmentType, deps: Dependencies): bg.LoggerPort {
-  const redactor = new bg.RedactorCompositeStrategy([
-    new bg.RedactorMetadataCompactArrayStrategy(),
-    new bg.RedactorMaskStrategy(bg.RedactorMaskStrategy.DEFAULT_KEYS),
+export function createLogger(Env: EnvironmentType, deps: Dependencies) {
+  const app = "lobbygow";
+
+  const redactor = new bg.RedactorComposite([
+    new bg.RedactorMetadataCompactArray({ maxItems: tools.IntegerPositive.parse(3) }),
+    new bg.RedactorMask(bg.RedactorMask.DEFAULT_KEYS),
   ]);
-  const diagnostics = new bg.WoodchopperDiagnosticsConsoleError(redactor);
+
+  const sampling = new bg.WoodchopperSamplingComposite([
+    new bg.WoodchopperSamplingPassLevel([bg.LogLevelEnum.error, bg.LogLevelEnum.warn, bg.LogLevelEnum.info]),
+    new bg.WoodchopperSamplingPassComponent(["infra", "security"]),
+    new bg.WoodchoperSamplingCorrelationId({ everyNth: tools.IntegerPositive.parse(10) }),
+    new bg.WoodchopperSamplingEveryNth({ n: tools.IntegerPositive.parse(10) }),
+  ]);
 
   return {
     [bg.NodeEnvironmentEnum.local]: new bg.Woodchopper(
       {
-        app: "lobbygow",
+        app,
         environment: Env.type,
         level: Env.LOGS_LEVEL,
         redactor,
         dispatcher: new bg.WoodchopperDispatcherAsync(new bg.WoodchopperSinkStdoutHuman()),
-        diagnostics,
+        diagnostics: new bg.WoodchopperDiagnosticsConsoleError(),
       },
       deps,
     ),
@@ -26,12 +35,15 @@ export function createLogger(Env: EnvironmentType, deps: Dependencies): bg.Logge
     [bg.NodeEnvironmentEnum.staging]: new bg.LoggerNoopAdapter(),
     [bg.NodeEnvironmentEnum.production]: new bg.Woodchopper(
       {
-        app: "lobbygow",
+        app,
         environment: Env.type,
         level: Env.LOGS_LEVEL,
         redactor,
-        dispatcher: new bg.WoodchopperDispatcherAsync(new bg.WoodchopperSinkStdout()),
-        diagnostics,
+        dispatcher: new bg.WoodchopperDispatcherSampling(
+          new bg.WoodchopperDispatcherAsync(new bg.WoodchopperSinkStdout()),
+          sampling,
+        ),
+        diagnostics: new bg.WoodchopperDiagnosticsConsoleError(redactor),
       },
       deps,
     ),

932a5f20eed70cdfaa37841712039905eeca3c9e
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-26 13:25:04 +0100

    step: simplify paths in the start script

--- scripts/production-server-start.sh
+++ scripts/production-server-start.sh
@@ -6,5 +6,6 @@ echo "Starting project..."
 export NODE_ENV="production"
 
 cd /var/www/lobbygow || exit
-/home/bgord/.bun/bin/bun install
-/home/bgord/.bun/bin/bun index.js
+
+bun install
+bun index.js

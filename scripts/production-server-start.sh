abbcb68568c2c91c19b921a9c62dc2d84b932d44
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-26 13:13:02 +0100

    refactor: install all packages in prod

--- scripts/production-server-start.sh
+++ scripts/production-server-start.sh
@@ -6,5 +6,5 @@ echo "Starting project..."
 export NODE_ENV="production"
 
 cd /var/www/lobbygow || exit
-/home/bgord/.bun/bin/bun install nodemailer
+/home/bgord/.bun/bin/bun install
 /home/bgord/.bun/bin/bun index.js

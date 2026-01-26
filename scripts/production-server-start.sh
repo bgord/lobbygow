2c68fcf7bc104b9571fa7ba2c80aae140b137dec
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-26 13:40:19 +0100

    step: improve the start script

--- scripts/production-server-start.sh
+++ scripts/production-server-start.sh
@@ -6,5 +6,5 @@ echo "Starting project..."
 export NODE_ENV="production"
 
 cd /var/www/lobbygow || exit
-/home/bgord/.bun/bin/bun install
+/home/bgord/.bun/bin/bun install --production --no-save --exact
 /home/bgord/.bun/bin/bun index.js

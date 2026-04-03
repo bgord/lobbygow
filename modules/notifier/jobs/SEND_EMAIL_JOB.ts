3b4223874a4695bcce364998d92e163493d75749
Bartosz Gordon <gordonbartosz@gmail.com>
2026-04-03 18:30:08 +0200

    step: update packages

--- modules/notifier/jobs/SEND_EMAIL_JOB.ts
+++ modules/notifier/jobs/SEND_EMAIL_JOB.ts
@@ -2,7 +2,9 @@ import * as bg from "@bgord/bun";
 import * as tools from "@bgord/tools";
 import * as v from "valibot";
 
+// Stryker disable all
 export const SEND_EMAIL_JOB = "SEND_EMAIL_JOB";
+// Stryker restore all
 
 export const SendEmailJobSchema = v.object({
   ...bg.JobEnvelopeSchema,

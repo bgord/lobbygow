df66e6ad3e1973dcee98833b893af6d78b57aa98
Bartosz Gordon <gordonbartosz@gmail.com>
2026-04-03 17:31:53 +0200

    step: add test clock adapter

--- infra/adapters/system/clock.adapter.ts
+++ infra/adapters/system/clock.adapter.ts
@@ -1,3 +1,14 @@
 import * as bg from "@bgord/bun";
+import * as tools from "@bgord/tools";
+import type { EnvironmentResultType } from "+infra/env";
 
-export const Clock = new bg.ClockSystemAdapter();
+const T0 = tools.Timestamp.fromNumber(Date.UTC(2025, 0, 1, 0, 0, 0));
+
+export function createClock(Env: EnvironmentResultType): bg.ClockPort {
+  return {
+    [bg.NodeEnvironmentEnum.local]: new bg.ClockSystemAdapter(),
+    [bg.NodeEnvironmentEnum.test]: new bg.ClockFixedAdapter(T0),
+    [bg.NodeEnvironmentEnum.staging]: new bg.ClockSystemAdapter(),
+    [bg.NodeEnvironmentEnum.production]: new bg.ClockSystemAdapter(),
+  }[Env.type];
+}

6b3aae8f46346d5daeb7ba26bccd723847c018c4
Bartosz Gordon <gordonbartosz@gmail.com>
2026-05-13 13:29:56 +0200

    step: update bun

--- infra/tools/prerequisites.ts
+++ infra/tools/prerequisites.ts
@@ -60,7 +60,7 @@ export function createPrerequisites(
       new bg.Prerequisite(
         "bun",
         new bg.PrerequisiteVerifierBunAdapter({
-          version: tools.PackageVersion.fromString("1.3.13"),
+          version: tools.PackageVersion.fromString("1.3.14"),
           current: Bun.version,
         }),
       ),

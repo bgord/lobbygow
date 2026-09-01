652592fd14e1e26678edc26e961b6aee5019e349
Bartosz Gordon <gordonbartosz@gmail.com>
2026-09-01 11:25:24 +0200

    Simplify error handler

--- app/http/error-handler.ts
+++ app/http/error-handler.ts
@@ -8,13 +8,11 @@ const validation = new bg.ErrorClassifierValidationStrategy([
   bg.MailerContentHtmlError,
 ]);
 
-const http = new bg.ErrorClassifierHttpExceptionHonoStrategy([bg.HttpExceptionErrors]);
-
 export class ErrorHandler {
   static handle: (deps: Dependencies) => hono.ErrorHandler = (deps) =>
     new bg.ErrorHonoHandler(
       [
-        http,
+        new bg.ErrorClassifierHttpExceptionHonoStrategy([bg.HttpExceptionErrors]),
         new bg.ErrorClassifierWithLoggerStrategy({ operation: "validation" }, { inner: validation, ...deps }),
       ],
       deps,
